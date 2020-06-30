3d79eaed29c057ea4713e86d90b1bea08d239674
Glitch (guvenli-bot-) <none>
2020-06-30 19:40:44 +0000

    🌎👍 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    "ownerID": "704048060107587682", //kendi IDınızı yazınız
+    "admins": ["704048060107587682"],
+    "support": ["İD"],
+    "token": "NzIyMTk5MzYxNTM0Mjk2MTY3.XvDbAw.Zv3QGFunV_18EnpsO8sAs609oAA", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "", //botunuzun secretini yazınız
+      "callbackURL": `https://vortex-altyaps.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "super-secret-session-thing", //kalsın
+      "domain": "S-1qdZhbU1AXWQC0ThNQ9bxPzbhhT42H", //site URLnizi yazınız!
+          "port": process.env.PORT
+    }
+  };
+  
+  module.exports = config;
+
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+
+//GİZLYİE ALMASANIZ SUNUCUNUZ PATLIYABİLİR
